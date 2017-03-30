8162ac3bfabea4ee564e6d977710c0dbf28eae53
haidong94 <haidong194@gmail.com>
2017-03-30 09:06:36 +0700

    model

---
+++ app/src/main/java/vinsoft/com/wavefindyourfriend/model/Person.java
@@ -0,0 +1,58 @@
+package vinsoft.com.wavefindyourfriend.model;
+
+/**
+ * Created by DONG on 28-Mar-17.
+ */
+
+public class Person {
+    int id;
+    String name;
+    String pass;
+    String sex;
+    String image;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+
+
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
